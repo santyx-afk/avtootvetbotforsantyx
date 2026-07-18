d71c3083ef85bebe7f566d75ab479303d43c9d92
santyx-afk <instalonerich@gmail.com>
2026-07-19 01:36:37 +0500

    Delete avtootvetbotforsantyx directory

--- avtootvetbotforsantyx/netlify/functions/admin-logout.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: {
-    'Set-Cookie': 'admin_session=; Path=/; Max-Age=0; HttpOnly; SameSite=Strict; Secure',
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ ok: true }),
-});
--- avtootvetbotforsantyx/netlify/functions/admin-session.js
+++
@@ -1,9 +0,0 @@
-const { requireAdmin } = require('../../shared/auth');
-
-exports.handler = async (event) => {
-  return {
-    statusCode: requireAdmin(event.headers) ? 200 : 401,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ok: requireAdmin(event.headers) }),
-  };
-};
